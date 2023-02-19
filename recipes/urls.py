a15226da3a16ba7079d4a8cf31654b37aaf3ed3c
Gabriel Holanda de Araújo <gholanda96@gmail.com>
2023-02-19 15:44:31 -0300

    Static Files

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from recipes.views import home
+from . import views
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>', views.recipes),
 ]
