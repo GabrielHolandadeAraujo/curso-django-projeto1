a6833feae590de0262d0c62e1ff3506addacdd02
Gabriel Holanda de Araújo <gholanda96@gmail.com>
2023-02-19 22:37:48 -0300

    Injeção de dados na Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipes-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
