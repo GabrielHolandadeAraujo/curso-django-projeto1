44c774218b5a359b28b33907d69ceac696ffd7d8
Gabriel Holanda de Araújo <gholanda96@gmail.com>
2023-02-21 12:21:44 -0300

    shortcut get_list_or_404 para urls que não existem

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from utils.recipes.factory import make_recipe
 
-from .models import Recipe
+from recipes.models import Recipe
 
 
 def home(request):
@@ -14,12 +14,16 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+    #Abrir tela de emojis é WINDOWS + . (ponto)
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
