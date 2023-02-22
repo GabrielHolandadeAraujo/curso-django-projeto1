6273470bd3402c990b4ba76842760afb148c2b18
Gabriel Holanda de Araújo <gholanda96@gmail.com>
2023-02-22 19:57:45 -0300

    Testando o 404

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -9,15 +9,27 @@ class RecipeViewsTest(TestCase):
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
-            reverse('recipes:category', kwargs={'category_id': 1})
+            reverse('recipes:category', kwargs={'category_id': 1000})
         )
         self.assertIs(view.func, views.category)
+
+    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
 
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
 
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
@@ -26,3 +38,12 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes found here 😢</h1>',
+            response.content.decode('utf-8')
+        )
+
+    
