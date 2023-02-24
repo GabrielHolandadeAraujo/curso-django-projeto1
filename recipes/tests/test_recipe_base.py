076b418bbba67ebece9af36dfffe1b6534126790
Gabriel Holanda de Araújo <gholanda96@gmail.com>
2023-02-23 22:10:24 -0300

    Criando fixtures para testes

---
+++ recipes/tests/test_recipe_base.py
@@ -0,0 +1,62 @@
+from django.test import TestCase
+from recipes.models import Category, Recipe, User
+
+class RecipeTestBase(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def make_category(self, name='Category'):
+        return Category.objects.create(name=name)
+    
+    def make_author(
+        self,
+        first_name='user',
+        last_name='name',
+        username='username',
+        password='123456',
+        email='username@email.com',
+    ):
+        return User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=email,
+        )
+    
+    def make_recipe(
+        self,
+        category_data=None,
+        author_data=None,
+        title='Recipe Title',
+        description='Recipe Description',
+        slug='recipe-slug',
+        preparation_time=10,
+        preparation_time_unit='Minutos',
+        servings=5,
+        servings_unit='Porções',
+        preparation_steps='Recipe Preparation Steps',
+        preparation_steps_is_html=False,
+        is_published=True,
+    ):
+        if category_data is None:
+            category_data = {}
+
+        if author_data is None:
+            author_data = {}
+
+        return Recipe.objects.create(
+            # The ** make each variable of the dictionary reciave the static value defined in the function
+            category=self.make_category(**category_data),
+            author=self.make_author(**author_data),
+            title=title,
+            description=description,    
+            slug=slug,
+            preparation_time=preparation_time,
+            preparation_time_unit=preparation_time_unit,
+            servings=servings,
+            servings_unit=servings_unit,
+            preparation_steps=preparation_steps,
+            preparation_steps_is_html=preparation_steps_is_html,
+            is_published=is_published,
+        )
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,8 +1,12 @@
-from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
+from .test_recipe_base import RecipeTestBase
 
-class RecipeViewsTest(TestCase):
+
+class RecipeViewsTest(RecipeTestBase):
+    def tearDown(self) -> None:
+        return super().tearDown()   
+
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -46,4 +50,14 @@ class RecipeViewsTest(TestCase):
             response.content.decode('utf-8')
         )
 
-    
+    def test_recipe_home_template_loads_recipes(self):
+        # Need a recipe for this test
+        self.make_recipe()
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        # Check if one recipe exists
+        self.assertIn('Recipe Title', content)
+        self.assertEqual(len(response_context_recipes), 1)
