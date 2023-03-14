9f0259e3e2496caa77c3168fc4a0ef156bc43525
Gabriel Holanda de Araújo <gholanda96@gmail.com>
2023-03-13 23:47:40 -0300

    Evitando reenvio de formulario

--- authors/urls.py
+++ authors/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'authors'
+
 urlpatterns = [
     # o path na url do app terá a url e a chamada da função criada para renderizar o template escolhido 
     # É necessário importar a viws do próprio app para ter aceso a função, o nome no terceiro argumento 
     # é opcional, mas serve para identificar o caminho, então é recomendado
     path('register/', views.register_view, name='register'),
+    path('register/create/', views.register_create, name='create'),
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
 from .forms import RegisterForm
+from django.http import Http404
 
 # Create your views here.
 
 # É necessário importar o redenr de django.shortcuts para renderizar a url, para isso precisamos de uma função 
 # que retorne o redenr passando a requisição e o caminho para o template em questão
 def register_view(request):
-    #  Aqui temos uma estrátegia de rota, se o form tiver o método POST, então enviaremos os dados do post na função
-     if request.POST:
-        form = RegisterForm(request.POST)
-    # Caso não tenha o post, a função não tem parametros
-    else:
-        form = RegisterForm()
-    # Aqui estamos renderizando a URL para o template e a request, também passamos o form via variável django
-    # se for método post
+    # Aqui temos duas possibilidades, ou recebe o parâmetro ou recebe none
+    register_form_data = request.session.get('register_form_data', None)
+    # Caso tenha parâmetro será get, caso não tenha será post
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
+
+
+def register_create(request):
+    # se o método for get (tentar reenviar um msm form) ele da um 404 
+    if not request.POST:
+        raise Http404()
+    #caso o método for post vai receber os dados e repassar para a função de view mandando get para evitar 
+    # o reenvio do form
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
